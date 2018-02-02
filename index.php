b74b84c697006ad19fb57a4f1f5a7df91e1847f5
DanielD3V <danieldevofficial@gmail.com>
2018-02-02 22:35:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,6 @@
 		text-align: center;
 		color: black;
 		font-family: arial;
-		border-top: 1px lightgray solid;
 		border-bottom: 1px lightgray solid;
 		padding: 16px 0px;
 	}
@@ -56,7 +55,7 @@
 	<center>
 	iPhone10,1, iOS 11.2.5
 	</center>
-	</div><br>
+	</div>
 
 	<!-- iPhone 10,4 iPhone 8 -->
 	<a href="check.php?device=iPhone10,1&boardconfig=d201ap&version=11.2.5">
@@ -64,7 +63,7 @@
 	<center>
 	iPhone10,4, iOS 11.2.5
 	</center>
-	</div><br>
+	</div>
 	
 	<!-- iPhone 8,4 iPhone SE (TSMC) -->
 	<a href="check.php?device=iPhone8,4&boardconfig=n69uap&version=11.2.5">
@@ -72,7 +71,7 @@
 	<center>
 	iPhone 8,4, iOS 11.2.5
 	</center>
-	</div><br>
+	</div>
 
 	<!-- iPhone 8,4 iPhone SE (Samsung)-->
 	<a href="check.php?device=iPhone8,4&boardconfig=n69ap&version=11.2.5">
@@ -80,7 +79,7 @@
 	<center>
 	iPhone 8,4, iOS 11.2.5
 	</center>
-	</div><br>iPhone 7:
+	</div>
 	
 	<!-- iPhone 9,1 iPhone 7 -->
 	<a href="check.php?device=iPhone9,1&boardconfig=d10ap&version=11.2.5">
@@ -88,8 +87,7 @@
 	<center>
 	iPhone 9,1, iOS 11.2.5 
 	</center>
-	</div><br>
-	iPhone 7:
+	</div>
 	
 	<!-- iPhone 8,1 iPhone 6s (TSMC) -->
 	<a href="check.php?device=iPhone8,1&boardconfig=n71map&version=11.2.5">
@@ -97,7 +95,7 @@
 	<center>
 	iPhone 8,1, iOS 11.2.5 
 	</center>
-	</div><br>
+	</div>
 	
 	<!-- iPhone 8,1 iPhone 6s (Samsung) -->
 	<a href="check.php?device=iPhone8,1&boardconfig=n71ap&version=11.2.5">
@@ -105,8 +103,7 @@
 	<center>
 	iPhone 8,1, iOS 11.2.5 
 	</center>
-	</div><br>
-	iPhone 7 Plus:	
+	</div>
 
 	<!-- iPhone 9,2 iPhone 7 Plus -->
 	<a href="check.php?device=iPhone9,1&boardconfig=d11ap&version=11.2.5">
@@ -114,8 +111,7 @@
 	<center>
 	iPhone 9,2, iOS 11.2.5 
 	</center>
-	</div><br>
-	iPhone 7 Plus:
+	</div>
 
 	<!-- iPhone 9,4 iPhone 7 Plus -->
 	<a href="check.php?device=iPhone9,4&boardconfig=d111ap&version=11.2.5">
@@ -123,8 +119,7 @@
 	<center>
 	iPhone 9,4, iOS 11.2.5 
 	</center>
-	</div><br>
-	iPhone 7:
+	</div>
 
 	<!-- iPhone 9,3 iPhone 7 -->
 	<a href="check.php?device=iPhone9,3&boardconfig=d101ap&version=11.2.5">
@@ -132,8 +127,7 @@
 	<center>
 	iPhone 9,3, iOS 11.2.5 
 	</center>
-	</div><br>
-	iPhone 6s:
+	</div>
 
 	<!-- iPhone 8,1 iPhone 6s (Samsung) -->
 	<a href="check.php?device=iPhone8,1&boardconfig=n71ap&version=11.2.5">
@@ -141,7 +135,7 @@
 	<center>
 	iPhone 8,1, iOS 11.2.5 
 	</center>
-	</div><br>
+	</div>
 	
 	<!-- iPhone 8,2 iPhone 6s  (TSMC) -->
 	<a href="check.php?device=iPhone8,1&boardconfig=n71map&version=11.2.5">
@@ -149,8 +143,7 @@
 	<center>
 	iPhone 8,1, iOS 11.2.5 
 	</center>
-	</div><br>
-	iPhone 6s Plus:
+	</div>
 
 	<!-- iPhone 8,2 iPhone 6s Plus (Samsung) -->
 	<a href="check.php?device=iPhone8,2&boardconfig=n66ap&version=11.2.5">
@@ -158,8 +151,7 @@
 	<center>
 	iPhone 8,2, iOS 11.2.5 
 	</center>
-	</div><br>
-	iPhone 6s Plus (TSMC):
+	</div>
 
 	<!-- iPhone 8,2 iPhone 6s Plus (TSMC) -->
 	<a href="check.php?device=iPhone8,2&boardconfig=n66map&version=11.2.5">
@@ -167,8 +159,7 @@
 	<center>
 	iPhone 8,2, iOS 11.2.5 
 	</center>
-	</div><br>
-	iPhone 6 Plus:
+	</div>
 
 	<!-- iPhone 7,1 iPhone 6 Plus -->
 	<a href="check.php?device=iPhone7,1&boardconfig=n56ap&version=11.2.5">
@@ -176,8 +167,7 @@
 	<center>
 	iPhone 7,1, iOS 11.2.5 
 	</center>
-	</div><br>
-	iPhone 6:
+	</div>
 
 	<!-- iPhone 7,2 iPhone 6 -->
 	<a href="check.php?device=iPhone7,2&boardconfig=n61ap&version=11.2.5">
@@ -185,15 +175,14 @@
 	<center>
 	iPhone 7,2, iOS 11.2.5 
 	</center>
-	</div><br>
-	
+	</div>
 	<!-- iPhone 6,1 iPhone 5s (GSM) -->
 	<a href="check.php?device=iPhone6,1&boardconfig=n51ap&version=11.2.5">
 	<div class="btn_bottom">
 	<center>
 	iPhone 6,1, iOS 11.2.5 
 	</center>
-	</div><br>
+	</div>
 	
 	<!-- iPhone 6,2 iPhone 5s (Global) -->
 	<a href="check.php?device=iPhone6,2&boardconfig=n53ap&version=11.2.5">
@@ -206,14 +195,12 @@
 	<!-- 32-Bit Devices Start Here -->
 	
 	<!-- iPhone 5,4 iPhone 5c -->
-	<br>
-	iPhone 5c:
 	<a href="check.php?device=iPhone5,4&boardconfig=n49app&version=10.3.3">
 	<div class="btn_bottom">
 	<center>
 	iPhone 5,4, iOS 10.3.3
 	</center>
-	</div><br>
+	</div>
 	
 	<!-- iPhone 5,3 iPhone 5c -->
 	<a href="check.php?device=iPhone5,3&boardconfig=n48ap&version=10.3.3">
@@ -221,7 +208,7 @@
 	<center>
 	iPhone 5,3, iOS 10.3.3
 	</center>
-	</div><br>
+	</div>
 	
 	<!-- iPhone 5,1 iPhone 5 -->
 	<a href="check.php?device=iPhone5,1&boardconfig=n41ap&version=10.3.3">
@@ -229,7 +216,7 @@
 	<center>
 	iPhone 5,1, iOS 10.3.3 
 	</center>
-	</div><br>
+	</div>
 	
 	<!-- iPhone 5,2 iPhone 5 -->
 	<a href="check.php?device=iPhone5,2&boardconfig=n42ap&version=10.3.3">
@@ -237,7 +224,7 @@
 	<center>
 	iPhone 5,2, iOS 10.3.3 
 	</center>
-	</div><br>
+	</div>
 
 	<!-- iPhone 4,1 iPhone 4s -->
 	<a href="check.php?device=iPhone4,1&boardconfig=n94ap&version=9.3.5">
@@ -245,7 +232,7 @@
 	<center>
 	iPhone 4,1, iOS 9.3.5 
 	</center>
-	</div><br>
+	</div>
 
 	<!-- iPhone 3,3 iPhone 4 (CDMA)-->
 	<a href="check.php?device=iPhone3,3&boardconfig=n92ap&version=7.1.2">
@@ -253,7 +240,7 @@
 	<center>
 	iPhone 3,3, iOS 7.1.2
 	</center>
-	</div><br>
+	</div>
 
 	<!-- iPhone 3,2 iPhone 4 (GSM / 2012) -->
 	<a href="check.php?device=iPhone3,2&boardconfig=n90bap&version=7.1.2">
@@ -261,7 +248,7 @@
 	<center>
 	iPhone 3,2, iOS 7.1.2
 	</center>
-	</div><br>
+	</div>
 
 	<!-- iPhone 3,1 iPhone 4 (GSM) -->
 	<a href="check.php?device=iPhone3,1&boardconfig=n90ap&version=7.1.2">
@@ -269,7 +256,7 @@
 	<center>
 	iPhone 3,1, iOS 7.1.2
 	</center>
-	</div><br>
+	</div>
 	
 	<!-- iPhone 2,1 iPhone 3GS -->
 	<a href="check.php?device=iPhone2,1&boardconfig=n88ap&version=6.1.6">
@@ -277,7 +264,7 @@
 	<center>
 	iPhone 2,1, iOS 6.1.6
 	</center>
-	</div><br>
+	</div>
 
 	<!-- iPhone 1,2 iPhone 3g -->
 	<a href="check.php?device=iPhone1,2&boardconfig=n82ap&version=4.2.1">
@@ -285,7 +272,7 @@
 	<center>
 	iPhone 1,2, iOS 4.2.1
 	</center>
-	</div><br>
+	</div>
 	
 	<!-- iPhone 1,1 iPhone 2g -->
 	<a href="check.php?device=iPhone1,1&boardconfig=n42ap&version=3.1.3">
