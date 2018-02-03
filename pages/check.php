3832b3aac8ad1976199303a7749b82448b404ab0
Felon <felon@Joes-iMac.local>
2018-02-03 21:33:28 +1100

    Different gradients for signed & unsigned

--- pages/check.php
+++ pages/check.php
@@ -59,29 +59,29 @@
     <!-- Scrollable page content-->
     <div class="page-content">
       <div class="content-block-title">Signing Status</div>
-      <?php if (strpos($device, "AppleTV") !== false): ?>
-        
+      <?php 
+        $output = shell_exec("cd ../internal_files; ./tsschecker/tsschecker_macos -d $device_clean --boardconfig $boardconfig_clean -i $version_clean");
+        $arr1 = explode(PHP_EOL, $output);
+        end($arr1);
+        $arr2 = prev($arr1);
+        // print_r($arr2);
+        if (GetBetween($arr2, "IS ", " signed!") == "NOT being") {
+          $signed_text = "is not signed!";
+          $signed = "no";
+        } elseif (GetBetween($arr2, "IS ", " signed!") == "being") {
+          $signed_text = "is signed!";
+          $signed = "yes";
+        } else {
+          $signed_text = "an unknown error occured!";
+          $signed = "no";
+        }
+      ?>
+      <?php if ($signed == "yes"): ?>
         <div class="card" style="background-image: linear-gradient(to right, #84c1ec, #66c4e7, #49c6dd, #36c6cc, #39c6b7); color: white;">
-
+        <?php else: ?>
+          <div class="card" style="background-image: linear-gradient(to right, #f28ca8, #f48a9f, #f58995, #f5898c, #f48982); color: white;">
       <?php endif ?>
 
-      <?php if (strpos($device, "iPod") !== false): ?>
-        
-        <div class="card" style="background-image: linear-gradient(to right, #c7a9ff, #baa6ff, #aca3fe, #9da0fd, #8d9dfc); color: white;">
-
-      <?php endif ?>
-
-      <?php if (strpos($device, "iPad") !== false): ?>
-        
-        <div class="card" style="background-image: linear-gradient(to right, #a626d0, #822dbf, #5f2eac, #3d2b97, #1a2680);); color: white;">
-
-      <?php endif ?>
-
-      <?php if (strpos($device, "iPhone") !== false): ?>
-
-        <div class="card" style="background-image: linear-gradient(to right, #f28ca8, #f48a9f, #f58995, #f5898c, #f48982); color: white;">
-
-      <?php endif ?>
           <div class="card-content">
               <div class="card-content-inner">
                 <span style="display:block; margin-left: 10px; margin-top: 5px; font-weight: 500; font-size: 20px;"><?php echo "$name"; ?></span>
@@ -92,20 +92,7 @@
                     <?php echo "on iOS $version"; ?></span>
                   <?php endif ?>
                 <span style="display:block; margin-left: 10px; margin-top: 5px; font-weight: 300; font-size: 13px;">
-                  <?php 
-                    $output = shell_exec("cd ../internal_files; ./tsschecker/tsschecker_macos -d $device_clean --boardconfig $boardconfig_clean -i $version_clean");
-                    $arr1 = explode(PHP_EOL, $output);
-                    end($arr1);
-                    $arr2 = prev($arr1);
-                    // print_r($arr2);
-                    if (GetBetween($arr2, "IS ", " signed!") == "NOT being") {
-                      echo "is not signed!";
-                    } elseif (GetBetween($arr2, "IS ", " signed!") == "being") {
-                      echo "is signed!";
-                    } else {
-                      echo "an unknown error occured!";
-                    }
-                  ?>
+                  <?php echo $signed_text; ?>
                 </span>
                 <img src="https://ipsw.me/api/images/320x/assets/images/devices/<?php echo $device; ?>.png" width="40px" height="auto" style="display: inline-block;position: absolute;top: 50%;transform: translateY(-50%);float: right;opacity: 0.3;filter: gray;-webkit-filter: grayscale(1);filter: grayscale(1);right: 10%;">
               </div>
